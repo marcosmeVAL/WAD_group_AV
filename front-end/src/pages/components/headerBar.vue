7205d64373b20c790cc8a4355b7bde52e82c7784
KaldaM <matteuskalda@gmail.com>
2025-12-15 20:15:41 +0200

    Added logout button and made it replace the login button when logged in

--- front-end/src/pages/components/headerBar.vue
+++ front-end/src/pages/components/headerBar.vue
@@ -5,15 +5,40 @@
 
             <router-link to="/" class="nav-link">Home</router-link>
             <router-link to="/contacts" class="nav-link">Contacts</router-link>
-            <router-link to="/login" class="nav-link">Login</router-link> 
+            <router-link v-if="!isAuth" to="/login" class="nav-link">Login</router-link>
+            <button v-else class="nav-link btn-link" @click="logout">Logout</button>
         </nav>
     </header>
 </template>
 
 <script>
+import { api } from "@/api";
+
 export default {
-    name: "headerBar"
-}
+  name: "headerBar",
+  data() {
+    return { isAuth: false };
+  },
+  async mounted() {
+    await this.refreshAuth();
+  },
+  watch: {
+    "$route.fullPath": async function () {
+      await this.refreshAuth();
+    }
+  },
+  methods: {
+    async refreshAuth() {
+      const r = await api("/auth/authenticate").catch(() => ({ authenticated: false }));
+      this.isAuth = !!r.authenticated;
+    },
+    async logout() {
+      await api("/auth/logout");
+      this.isAuth = false;
+      this.$router.push({ name: "Login" });
+    }
+  }
+};
 </script>
 
 <style>
@@ -41,5 +66,17 @@ export default {
 }
 .nav-link.router-link:active {
     background: #640764ff;
+}
+.nav-button {
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  font: inherit;
+  color: inherit;
+}
+.btn-link {
+  border: none;
+  background: transparent;
+  cursor: pointer;
 }
 </style>
